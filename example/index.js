df6e36166376867e9ad3059dd22e8849fcba4693
Kristóf Poduszló <kripod@protonmail.com>
2016-05-13 12:18:19 +0200

    Drastically improved key generation performance

--- example/index.js
+++ example/index.js
@@ -1,17 +1,8 @@
 import ElGamal from './../src';
-import forge from 'node-forge';
-import { BigInteger } from 'jsbn';
-
-/*
-// 55212756559928793179526928356344250630685495376116389993318240227888367461537
-const bi = new BigInteger('55212756559928793179526928356344250630685495376116389993318240227888367461537');
-console.log(bi.toString());
-console.log(bi.shiftLeft(1).add(BigInteger.ONE).toString());
-*/
 
 async function run() {
   console.time('example');
-  const eg = await ElGamal.generateAsync(256);
+  const eg = await ElGamal.generateAsync(2048);
   console.timeEnd('example');
 }
 
--- src/utils.js
+++ src/utils.js
@@ -1,25 +1,31 @@
 import Promise from 'bluebird';
 import crypto from 'crypto';
 import { BigInteger } from 'jsbn';
-import { prime } from 'node-forge';
 
 Promise.promisifyAll(crypto);
-Promise.promisifyAll(prime);
+
+const BIG_TWO = new BigInteger('2');
 
 /**
- * Returns a random prime BigInteger value.
- * @param {number} bits Number of bits in the output.
+ * Returns a random BigInteger with the given amount of bits.
+ * @param {BigInteger} bits Number of bits in the output.
  * @returns {BigInteger}
  */
-export async function getBigPrimeAsync(bits) {
-  const bi = await prime.generateProbablePrimeAsync(bits);
-  return new BigInteger(bi.toString());
+export async function getRandomNbitBigIntegerAsync(bits) {
+  // Generate random bytes with the length of the range
+  const buf = await crypto.randomBytesAsync(Math.ceil(bits / 8));
+  const bi = new BigInteger(buf.toString('hex'), 16);
+
+  // Trim the result if necessary and then ensure that the highest bit is set
+  const trimLength = bi.bitLength() - bits;
+  return bi.shiftRight(trimLength > 0 ? trimLength : 0).setBit(bits - 1);
 }
 
 /**
  * Returns a random BigInteger in the given range.
  * @param {BigInteger} min Minimum value (included).
  * @param {BigInteger} max Maximum value (excluded).
+ * @returns {BigInteger}
  */
 export async function getRandomBigIntegerAsync(min, max) {
   const range = max.subtract(min).subtract(BigInteger.ONE);
@@ -32,4 +38,25 @@ export async function getRandomBigIntegerAsync(min, max) {
 
   // Ensure that the generated value satisfies the given range
   return bi.compareTo(max) < 0 ? bi : getRandomBigIntegerAsync(min, max);
+}
+
+/**
+ * Returns a random prime BigInteger value.
+ * @param {number} bits Number of bits in the output.
+ * @returns {BigInteger}
+ */
+export async function getBigPrimeAsync(bits) {
+  // Generate a random odd number with the given length
+  let bi = (await getRandomNbitBigIntegerAsync(bits)).or(BigInteger.ONE);
+
+  while (!bi.isProbablePrime()) {
+    bi = bi.add(BIG_TWO);
+
+    // Sanity check for correct bit length
+    if (bi.bitLength() !== bits) {
+      return getBigPrimeAsync(bits);
+    }
+  }
+
+  return bi;
 }
